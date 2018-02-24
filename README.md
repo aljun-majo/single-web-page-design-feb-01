72d43932b7f9d1c2fd2813eb5018578ab1269eae
aljun m <aljunma20@gmail.com>
2018-02-24 16:57:20 +0800

    intialize repo
    intialize repo

---
+++ README.md
@@ -0,0 +1,5 @@
+# Single Page Web Design
+Personal Design
+- HTML, CSS, JavaScript, Jqeury, Bootstrap 4
+
+
---
+++ components/scripts/script.js
@@ -0,0 +1,10 @@
+$(function() {
+
+	var topoffset = 888888; //variable for scrolling effects
+	console.log(topoffset); 
+
+	$('.carousel').carousel({
+		interval: 2000
+	})
+
+  }); //function
